108eced591546669e9cb201bcb6b75e0a3a68c0f
Cameron Maclean <cm.maclean@yahoo.com>
2020-11-23 17:10:09 +0000

    Git Upload 2 - Finished tasks

--- marvel.sql
+++ marvel.sql
@@ -61,18 +61,30 @@ SELECT name FROM people;
 
 -- 3. Amend Krusty The Clown's name error
 
-UPDATE people SET name = 'Krusty the Clown' WHERE name = 'Crusty the Clown';
+UPDATE people SET name = 'Krusty the Clown' WHERE id = 13;
 
 -- 4. Return ONLY Homer Simpson's name from the 'people' table.
 
+SELECT name from people WHERE id = 1;
+
 -- 5. The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
+
+DELETE FROM movies WHERE title = 'Batman Begins';
 
 -- 6. We forgot one of the main characters! Add Bart Simpson to the 'people' table
 
+INSERT INTO people (name) VALUES ('Bart Simpson');
+
 -- 7. Eric Cartman has decided to hijack our movie evening, Remove him from the table of people.
 
--- 8. The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new 					entry in the 'movies' table to reflect this.
+DELETE FROM people WHERE name = 'Eric Cartman';
 
--- 9. The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show 					time of "Iron Man 3" to start two hours later.
+-- 8. The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! 				Create a new entry in the 'movies' table to reflect this.
 
+INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2018, '00:00');
+
+-- 9. The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and 					set the show time of "Iron Man 3" to start two hours later.
+
+SELECT show_time FROM movies WHERE title = 'Iron Man 2';
+UPDATE movies SET show_time = '20:45' WHERE title = 'Iron Man 2';
 
